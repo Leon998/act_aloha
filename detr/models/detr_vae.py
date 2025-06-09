610a79fce99db4fc1879c65c89a97b16800db4c5
Leon998 <1158046368@qq.com>
2025-06-09 16:52:05 +0800

    more annotation

--- detr/models/detr_vae.py
+++ detr/models/detr_vae.py
@@ -137,8 +137,8 @@ class DETRVAE(nn.Module):
             env_state = self.input_proj_env_state(env_state)
             transformer_input = torch.cat([qpos, env_state], axis=1) # seq length = 2
             hs = self.transformer(transformer_input, None, self.query_embed.weight, self.pos.weight)[0]
-        a_hat = self.action_head(hs)
-        is_pad_hat = self.is_pad_head(hs)
+        a_hat = self.action_head(hs)  # [8, 100, 14]
+        is_pad_hat = self.is_pad_head(hs)  # [8, 100, 1]
         return a_hat, is_pad_hat, [mu, logvar]
 
 
